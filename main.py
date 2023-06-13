69cee58ae85fe5264576d13d8139d4b5b75b2023
Prajwal <prajwalgbdr03@gmail.com>
2023-06-13 20:55:41 +0530

    Data transformation added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from textSummarizer.logging import logger
 
 
@@ -20,6 +21,17 @@ try:
     logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<")
     data_validation = DataValidationTrainingPipeline()
     data_validation.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    data_Transformation = DataTransformationTrainingPipeline()
+    data_Transformation.main()
     logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx========x")
 except Exception as e:
     logger.exception(e)
